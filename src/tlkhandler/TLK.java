cc94ac4f241e79e08d1e574990a8d1879884ebf9
Orest Voloshchuk <orest.v@gmail.com>
2012-09-18 11:13:53 +0300

    First commit

---
+++ src/tlkhandler/TLK.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlkhandler;
+
+/**
+ *
+ * @author ovoloshchuk
+ */
+public class TLK {
+    private String _filename;
+    
+    public TLK(String sFilename) {
+        _filename = sFilename;
+    }
+    
+    public String getFilename() {
+        return _filename;
+    }
+}
---
+++ src/tlkhandler/TlkHandler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlkhandler;
+
+/**
+ *
+ * @author ovoloshchuk
+ */
+public class TlkHandler {
+    public final static String TLK_FILENAME = "D:\\dialog.tlk";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
